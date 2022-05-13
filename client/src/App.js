4b1a46417add5e2110d394ef138347403fc12f99
Sive <sisandla@student.wethinkcode.co.za>
2022-05-13 02:10:22 +0200

    modified App file

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,7 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
-
 import MovieAPI from './api/MovieAPI';
-
 import MovieForm from './components/MovieForm';
 import MovieTable from './components/MovieTable';
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
